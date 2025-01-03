f80f3a08d4db050512b2616f12c89f5537041a66
nishagii <jeewanthadeherath04@gmail.com>
2025-01-03 13:54:32 +0530

    changed project files

--- demo/src/main/java/com/example/demo/repo/UserRepo.java
+++ demo/src/main/java/com/example/demo/repo/UserRepo.java
@@ -1,5 +1,7 @@
 package com.example.demo.repo;
 
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
